70278bde43aa471d2efc117189aeb2086d0c5598
mariojuk <mariojuk45@gmail.com>
2018-12-18 13:10:03 +0100

    admin home style

--- frontend/home/homeController.js
+++ frontend/home/homeController.js
@@ -1,6 +1,13 @@
 angular.module('SportApp')
 .controller('homeController', ['$scope', '$location', '$http', 'toastr', 'AuthService', function ($scope, $location, $http, toastr, AuthService){
 
-    $scope.user = AuthService.getCurrentUser(window.localStorage.getItem('sportApp'))
-    
+    $scope.user = AuthService.getCurrentUser(window.localStorage.getItem('sportApp')) //?
+    $scope.myFunction = function(){
+		  var x = document.getElementById("myAdminNav");
+		  if (x.className === "adminNav") {
+		    x.className += " responsive";
+		  } else {
+		    x.className = "adminNav";
+		  }
+	}
 }])
