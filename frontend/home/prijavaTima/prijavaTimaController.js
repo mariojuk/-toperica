57e89c21cb6bcfa2e1bd89bd65aeec3a1c2cc89c
mariojuk <mariojuk45@gmail.com>
2018-12-19 16:50:33 +0100

    registration team style

--- frontend/home/prijavaTima/prijavaTimaController.js
+++ frontend/home/prijavaTima/prijavaTimaController.js
@@ -7,15 +7,10 @@ angular.module('SportApp')
 			$http.get("api/create/findTeam/" + teamName).then (function(response){
 			$scope.showMeCompetitions=true;
 			$scope.allTeams=response.data
-
 		}) .catch(err => toastr.error(err.data))
-	
-
 	}
 		$http.get("api/create/findCompetitions").then (function(response){
 			$scope.allCompetitions=response.data
-      console.log(allCompetitions)
-
 		})
 
 	$scope.selectedTim=[]
@@ -28,9 +23,11 @@ angular.module('SportApp')
   		var idx=$scope.selected.indexOf(item._id)
   		if(idx > -1){
    			$scope.selected.splice(idx,1);
+        registration.team();
   		}
   		else{
    			$scope.selected.push(item._id)
+        registration.team();
   		}
  	}
  		$scope.existTeam=function(item){
@@ -41,12 +38,21 @@ angular.module('SportApp')
   		var idx=$scope.selectedTim.indexOf(item._id)
   		if(idx > -1){
    			$scope.selectedTim.splice(idx,1);
+        registration.team();
   		}
   		else{
-   			$scope.selectedTim.push(item._id)
+   			$scope.selectedTim.push(item._id);
+        registration.team();
   		}
-  		
  	}
+  var registration = this;
+  registration.team=function(){
+  if($scope.selectedTim.length == "0" || $scope.selected.length == "0"){
+      $scope.save = false
+  }else{
+      $scope.save = true
+    }
+  }
 
  	$scope.registerOnCompetition=function(){
  		$scope.timeId=$scope.selectedTim[0];
